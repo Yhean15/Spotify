1c494667f9dcad924ada732218b18820162815bd
Yhean15 <163749688+Yhean15@users.noreply.github.com>
2024-05-15 20:35:45 -0400

    Add files via upload

---
+++ Nodo/Songs.java
@@ -0,0 +1,13 @@
+package Nodo;
+import Elementos.Song;
+
+public class Songs {
+
+    public Song Songs;
+    public Nodo.Songs Next;
+
+    public Songs(Song Songs){
+        this.Songs=Songs;
+    }
+
+}
