e3e89589dc1ecfc13298b3a6e8c50fc0a51e00ba
Yhean15 <163749688+Yhean15@users.noreply.github.com>
2024-05-15 20:35:05 -0400

    Add files via upload

---
+++ Elementos/Song.java
@@ -0,0 +1,14 @@
+package Elementos;
+
+public class Song {
+    public String Nombre;
+    public String Album;
+    public String Autor;
+    public double duracion;
+    public Song(String Nombre, String Album, String artista, double duracion){
+        this.Nombre=Nombre;
+        this.Album=Album;
+        this.Autor=artista;
+        this.duracion=duracion;
+    }
+}
