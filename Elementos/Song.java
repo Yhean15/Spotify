7c0b5e4fe24fc4fa0ac6b77bb8830b0542a112d6
Yhean15 <163749688+Yhean15@users.noreply.github.com>
2024-05-17 13:10:22 -0400

    Update Song.java

--- Elementos/Song.java
+++ Elementos/Song.java
@@ -2,12 +2,10 @@ package Elementos;
 
 public class Song {
     public String Nombre;
-    public String Album;
     public String Autor;
     public double duracion;
-    public Song(String Nombre, String Album, String artista, double duracion){
+    public Song(String Nombre, String artista, double duracion){
         this.Nombre=Nombre;
-        this.Album=Album;
         this.Autor=artista;
         this.duracion=duracion;
     }
